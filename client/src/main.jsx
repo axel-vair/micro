f7724067e37c87eac0ea448f2caab9216d95e44e
Axel <axel@Macbook.local>
2024-04-26 20:26:29 +0200

    import lib react-calendar & add component calendar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from "./components/login.jsx";
 import Register from "./components/register.jsx";
+import Calendar from "./components/calendar.jsx";
+import './assets/calendar.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Register />
       <Login />
+      <Calendar />
   </React.StrictMode>,
 )
