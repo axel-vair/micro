3c9c1eaf142c79dc33a1f15b280633b1c59e4c7c
Axel <axel@Macbook.local>
2024-04-27 14:19:01 +0200

    add table component in my commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import Login from "./components/login.jsx";
 import Register from "./components/register.jsx";
 import Calendar from "./components/calendar.jsx";
 import './assets/calendar.css';
+import Table from "./components/table.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Register />
       <Login />
       <Calendar />
+      <Table />
   </React.StrictMode>,
 )
