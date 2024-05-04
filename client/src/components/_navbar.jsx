76287bd3e39fc08cd9129d3b259db4384aacf267
Axel <axel@Macbook.local>
2024-05-04 14:46:14 +0200

    add random design navbar

--- client/src/components/_navbar.jsx
+++ client/src/components/_navbar.jsx
@@ -2,14 +2,34 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Accueil</Link></li>
-                <li><Link to="/login">Connexion</Link></li>
-                <li><Link to="/register">Inscription</Link></li>
-                <li><Link to="/bookings">Réservations</Link></li>
-                <li><Link to="/book">Formulaire de réservation</Link></li>
-            </ul>
+        <nav className="bg-gray-800 py-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-white font-bold text-lg">
+                    <Link to="/">Accueil</Link>
+                </div>
+                <ul className="flex space-x-6 text-gray-400 transition-colors duration-300">
+                    <li>
+                        <Link to="/login" className="hover:text-white">
+                            Connexion
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/register" className="hover:text-white">
+                            Inscription
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/bookings" className="hover:text-white">
+                            Réservations
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/book" className="hover:text-white">
+                            Formulaire de réservation
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
