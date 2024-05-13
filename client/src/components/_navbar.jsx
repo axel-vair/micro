bfd09dc8d97153d49a9cd436aa86a62185bfd897
Axel <axel@Macbook.local>
2024-05-13 14:41:59 +0200

    push for diane

--- client/src/components/_navbar.jsx
+++ client/src/components/_navbar.jsx
@@ -5,7 +5,7 @@ const Navigation = () => {
         <nav className="bg-gray-800 py-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-white font-bold text-lg">
-                    <Link to="/">Accueil</Link>
+                    <Link to="/">Saveurs Modernes</Link>
                 </div>
                 <ul className="flex space-x-6 text-gray-400 transition-colors duration-300">
                     <li>
