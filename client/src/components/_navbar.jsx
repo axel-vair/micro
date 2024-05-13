c2f523dcb883ce4612e36d556b3617b380f73aee
Axel <axel@Macbook.local>
2024-05-13 16:35:37 +0200

    add nav condition

--- client/src/components/_navbar.jsx
+++ client/src/components/_navbar.jsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const isLoggedIn = !!user;
+
     return (
         <nav className="bg-gray-800 py-4">
             <div className="container mx-auto flex justify-between items-center">
@@ -8,26 +11,39 @@ const Navigation = () => {
                     <Link to="/">Saveurs Modernes</Link>
                 </div>
                 <ul className="flex space-x-6 text-gray-400 transition-colors duration-300">
-                    <li>
-                        <Link to="/login" className="hover:text-white">
-                            Connexion
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/register" className="hover:text-white">
-                            Inscription
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/bookings" className="hover:text-white">
-                            Réservations
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/book" className="hover:text-white">
-                            Formulaire de réservation
-                        </Link>
-                    </li>
+                    {!isLoggedIn && (
+                        <>
+                            <li>
+                                <Link to="/login" className="hover:text-white">
+                                    Connexion
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/register" className="hover:text-white">
+                                    Inscription
+                                </Link>
+                            </li>
+                        </>
+                    )}
+                    {isLoggedIn && (
+                        <>
+                            <li>
+                                <Link to="/bookings" className="hover:text-white">
+                                    Réservations
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/book" className="hover:text-white">
+                                    Formulaire de réservation
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/logout" className="hover:text-white">
+                                    Déconnexion
+                                </Link>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </div>
         </nav>
