ce0745cf57cde6a467e5c93f638ac38ce9910332
mehdi romdhani <mehdi.romdhani@laplateforme.io>
2024-05-15 11:22:36 +0200

    set admin panel w/ id

--- client/src/components/bookings.jsx
+++ client/src/components/bookings.jsx
@@ -7,12 +7,17 @@ const BookingList = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    const handleRowClick = ($id) =>{
+        console.log(`chaque id est : ${$id}`);
+    }
+
     useEffect(() => {
         const fetchBookings = async () => {
             try {
                 const response = await axios.get('http://localhost:3001/api/bookings/allBookings');
                 setBookings(response.data);
                 setLoading(false);
+                console.log(response.data);
             } catch (err) {
                 setError(err.message);
                 setLoading(false);
@@ -44,20 +49,44 @@ const BookingList = () => {
                             <th className="px-4 py-2">Heure de début</th>
                             <th className="px-4 py-2">Heure de fin</th>
                             <th className="px-4 py-2">Statut</th>
-                            <th className=''></th>
+                            <th className="px-4 py-2">Annulation</th>
+
                         </tr>
                         </thead>
                         <tbody>
-                        {bookings.map((booking) => (
-                            <tr key={booking._id} className="border-b border-gray-700">
-                                <td className="px-4 py-2">{booking.user?.email || 'Utilisateur inconnu'}</td>
-                                <td className="px-4 py-2">{new Date(booking.date).toLocaleDateString()}</td>
-                                <td className="px-4 py-2">{new Date(booking.startTime).toLocaleTimeString()}</td>
-                                <td className="px-4 py-2">{new Date(booking.endTime).toLocaleTimeString()}</td>
-                                <td className="px-4 py-2">{booking.status}</td>
-                            </tr>
-                        ))}
-                        </tbody>
+                            {bookings.map((booking) => (
+                                <tr
+                                key={booking._id}
+                                className={`border-b border-gray-700 ${
+                                    booking.status === true ? 'bg-green-500' : ''
+                                }`}
+                                >
+                                <td className="px-4 py-2">
+                                    {booking.user?.email || 'Utilisateur inconnu'}
+                                </td>
+                                <td className="px-4 py-2">
+                                    {new Date(booking.date).toLocaleDateString()}
+                                </td>
+                                <td className="px-4 py-2">
+                                    {new Date(booking.startTime).toLocaleTimeString()}
+                                </td>
+                                <td className="px-4 py-2">
+                                    {new Date(booking.endTime).toLocaleTimeString()}
+                                </td>
+                                <td className="px-4 py-2">
+                                    {booking.status === true ? 'Confirmé' : 'Annuler'}
+                                </td>
+                                <td className="px-4 py-2">
+                                    <button
+                                    type="button"
+                                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                                    onClick={()=>handleRowClick(booking._id)}>
+                                    Annuler
+                                    </button>
+                                </td>
+                                </tr>
+                            ))}
+                            </tbody>
                     </table>
                 )}
             </div>
