0f969f5ccc91e68a0449b5695e5fbea9fd5d697a
mehdi romdhani <mehdi.romdhani@laplateforme.io>
2024-05-16 10:38:54 +0200

    DEV{+++} end to updatePeople features

--- server/routes/booking.js
+++ server/routes/booking.js
@@ -5,5 +5,6 @@ const bookingController = require('../controllers/bookingController');
 router.post('/newBooking', bookingController.createBooking)
 router.get('/allBookings', bookingController.getAllBookings);
 router.patch('/:id/status', bookingController.updateBookingStatus);
+router.patch('/:id/people',bookingController.updateBookingPeople);
 module.exports = router;
 
